84db3c2c6de1c329bc348bc4d763bca77b12cc8b
fcordonezo <fabiancamilo960720@gmail.com>
2025-03-13 23:14:35 -0500

    APTV-12 feat(Model): Tournament model was created

---
+++ domain/model/src/main/java/com/fc2o/model/tournament/Tournament.java
@@ -0,0 +1,14 @@
+package com.fc2o.model.tournament;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+//import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+//@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Tournament {
+}
