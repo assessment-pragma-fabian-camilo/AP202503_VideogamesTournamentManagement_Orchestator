8a59db527b273764bcd1db1e572aa805183ffdfd
fcordonezo <fabiancamilo960720@gmail.com>
2025-03-13 22:55:17 -0500

    feat: Base structure created. Clean Arch Scaffold BC

---
+++ applications/app-service/src/main/java/com/fc2o/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package com.fc2o.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "com.fc2o.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
