9f399d5bc3af597b3a2585f16429ba6fa2d50a34
fcordonezo <fabiancamilo960720@gmail.com>
2025-03-24 22:19:19 -0500

    style(app): Generic Use Case config styled with 2 spaces

--- applications/app-service/src/main/java/com/fc2o/config/UseCasesConfig.java
+++ applications/app-service/src/main/java/com/fc2o/config/UseCasesConfig.java
@@ -6,9 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(basePackages = "com.fc2o.usecase",
-        includeFilters = {
-                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
-        },
-        useDefaultFilters = false)
+  includeFilters = {
+    @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+  },
+  useDefaultFilters = false)
 public class UseCasesConfig {
 }
