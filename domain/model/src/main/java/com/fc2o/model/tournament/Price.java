3fd656ba36922ba2da9c62b217ae02e4698c6125
fcordonezo <fabiancamilo960720@gmail.com>
2025-03-29 16:10:22 -0500

    fix(model): Modifications to models done by creation of entry point

--- domain/model/src/main/java/com/fc2o/model/tournament/Price.java
+++ domain/model/src/main/java/com/fc2o/model/tournament/Price.java
@@ -1,7 +1,10 @@
 package com.fc2o.model.tournament;
 
+import lombok.Builder;
+
 import java.math.BigDecimal;
 
+@Builder
 public record Price(
   BigDecimal participation,
   BigDecimal view
--- domain/model/src/main/java/com/fc2o/model/tournament/Transmission.java
+++ domain/model/src/main/java/com/fc2o/model/tournament/Transmission.java
@@ -1,5 +1,8 @@
 package com.fc2o.model.tournament;
 
+import lombok.Builder;
+
+@Builder
 public record Transmission(
   String url,
   Platform platform
